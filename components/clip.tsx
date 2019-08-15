1fd2140adedb271935aa9bc4f96f16e9249df99c
Matt Moriarity <matt@mattmoriarity.com>
2019-08-14 19:42:15 -0600

    Add a button to replay the clip

--- components/clip.tsx
+++ components/clip.tsx
@@ -1,14 +1,17 @@
+import React from "react"
 import {
   Typography,
   Card,
   makeStyles,
   Theme,
   createStyles,
-  CardContent
+  CardContent,
+  Button
 } from "@material-ui/core"
-import { VideoCardMedia } from "./video"
+import { VideoCardMedia, VideoCardMediaRef } from "./video"
 import { ClipDetailsFragment } from "../lib/generated/graphql-components"
 import { QuoteText } from "./quote"
+import ReplayIcon from "@material-ui/icons/Replay"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -30,13 +33,32 @@ interface Props {
 export const Clip: React.FC<Props> = ({ clip }) => {
   const classes = useStyles({})
 
+  const videoRef = React.useRef<VideoCardMediaRef>(null)
+
   return (
     <Card className={classes.card}>
-      <VideoCardMedia video={clip.video} autoplay start={clip.start} />
+      <VideoCardMedia
+        ref={videoRef}
+        video={clip.video}
+        autoplay
+        start={clip.start}
+      />
       <CardContent className={classes.content}>
         <Typography variant="body1" component="div">
           <QuoteText>{clip.quote}</QuoteText>
         </Typography>
+        <Button
+          style={{ marginTop: 8 }}
+          color="primary"
+          onClick={() => {
+            if (videoRef.current) {
+              videoRef.current.seekTo(clip.start)
+            }
+          }}
+        >
+          <ReplayIcon style={{ marginRight: 4 }} />
+          Replay
+        </Button>
       </CardContent>
     </Card>
   )
